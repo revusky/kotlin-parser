e64abbb40e677e73e5a6eac628731299b463e414
revusky <revusky@gmail.com>
2026-08-01 20:04:01 +0200

    Inching forward, can parse some stuff

--- src/ccc/KParse.java
+++ src/ccc/KParse.java
@@ -5,9 +5,19 @@ import kotlinparser.*;
 FileSystem fileSystem = FileSystems.getDefault();
 
 public void main(String[] args) throws IOException {
-    //Path path = fileSystem.getPath(args[0]);
-    Path path = fileSystem.getPath("Hello.kt");
-    parseFile(path);
+    if (args.length == 0) {
+        parseFile(fileSystem.getPath("Hello.kt"));
+    }
+    else for (String arg : args) {
+        Path path = fileSystem.getPath(arg);
+        try {
+            parseFile(path);
+            System.out.println("File " + path + " parsed successfully.");
+        } catch (ParseException pe) {
+            System.out.println("Failed on " + path);
+            pe.printStackTrace();
+        }
+    }
 }
 
 void parseFile(Path path) throws IOException{
