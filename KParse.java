e24f097519e09c3bb48b72fb06dda652d227cc48
revusky <revusky@gmail.com>
2026-08-02 14:47:56 +0200

    Adjusting the test harness KParse.java so that it compiles with older JDK's

--- KParse.java
+++ KParse.java
@@ -1,27 +1,37 @@
 import java.io.IOException;
 import org.parsers.kotlin.*;
+import java.nio.file.Files;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
-FileSystem fileSystem = FileSystems.getDefault();
+public class KParse {
+    static FileSystem fileSystem = FileSystems.getDefault();
 
-public void main(String[] args) throws IOException {
-    if (args.length == 0) {
-        parseFile(fileSystem.getPath("Hello.kt"));
-    }
-    else for (String arg : args) {
-        Path path = fileSystem.getPath(arg);
-        try {
-            parseFile(path);
-            System.out.println("File " + path + " parsed successfully.");
-        } catch (ParseException pe) {
-            System.out.println("Failed on " + path);
-            pe.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            usage();
+        }
+        for (String arg : args) {
+            Path path = fileSystem.getPath(arg);
+            try {
+                parseFile(path, args.length==1);
+                System.out.println("File " + path + " parsed successfully.");
+            } catch (ParseException pe) {
+                System.out.println("Failed on " + path);
+                pe.printStackTrace();
+            }
         }
     }
-}
 
-void parseFile(Path path) throws IOException{
-    KotlinParser parser = new KotlinParser(path);
-    parser.KotlinFile();
-    Node root = parser.rootNode();
-    root.dump();
+    static void usage() {
+        System.out.println("Usage java KParse <files>");
+    }
+
+    static void parseFile(Path path, boolean dump) throws IOException {
+        KotlinParser parser = new KotlinParser(path);
+        parser.KotlinFile();
+        Node root = parser.rootNode();
+        if (dump) root.dump();
+    }
 }
